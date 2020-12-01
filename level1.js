e3a7ce1b32a423f7612b4e4c4642d894b47bc8d6
Divyesh Parmar <divyeshpar2496@gmail.com>
2020-12-02 00:50:41 +0530

    verification check existing l1 tags before posting

--- level1.js
+++ level1.js
@@ -1,10 +1,12 @@
 const xlsxFile = require('read-excel-file/node');
+const colors = require('colors');
 const fetch = require('node-fetch');
 const L0TagsUrl = 'https://test-bff.airmeet.com/api/v1/tag/?tagType=LEVEL0';
+const L1TagsUrl = 'https://test-bff.airmeet.com/api/v1/tag/?tagType=LEVEL1';
 const postL1TagsUrl = 'https://test-bff.airmeet.com/api/v1/tag';
 
 // the logic for reading Excel sheet
-let newL2Tags = xlsxFile('../newtags.xlsx', { sheet: 'Level2' }).then((rows) => rows);
+// let newL2Tags = xlsxFile('../newtags.xlsx', { sheet: 'Level2' }).then((rows) => rows);
 let newL1Tags = xlsxFile('../newtags.xlsx', { sheet: 'Sheet5' }).then(
   (rows) => rows,
   (err) => console.warn(err)
@@ -20,6 +22,26 @@ const getL0Tags = async (url) => {
   } catch (error) {
     console.log('error in code for getL0Tags', error);
   }
+};
+
+const getL1Tags = async (url) => {
+  console.log(colors.bold.bgRed.yellow('first ... wait ... fetching Level-1 tags first...'));
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    // console.log(json);
+    return json;
+  } catch (error) {
+    console.log('error in code for getL1Tags', error);
+  }
+};
+
+const verifyL1Tags = async (existingL1Tags) => {
+  let pureNewL1Tags = await newL1Tags;
+  existingL1Tags.forEach((e) => {
+    pureNewL1Tags.filter((it) => it[1] !== e.name);
+  });
+  return pureNewL1Tags;
 };
 
 const postL1Tags = async (url, dataToSend = []) => {
@@ -39,17 +61,16 @@ const postL1Tags = async (url, dataToSend = []) => {
   }
 };
 
-const attachNewL1TagsWithL0Tags = async (currentlyFetchedL0Tags = []) => {
+const attachNewL1TagsWithL0Tags = async (l1Tags, currentlyFetchedL0Tags = []) => {
   console.log('second .... wait... attaching the Level tags with their parent IDs....');
-  const resolveNewL1Tags = await newL1Tags;
+  // const resolveNewL1Tags = l1Tags;
   const l1TagsWithParentId = [];
-  resolveNewL1Tags &&
-    resolveNewL1Tags.forEach((x, idx) => {
-      currentlyFetchedL0Tags.forEach((y, yidx) => {
-        x[1].trim().toLowerCase() === y.name.trim().toLowerCase() &&
-          l1TagsWithParentId.push({ type: x[0], name: x[2], parentId: y.id });
-      });
+  l1Tags.forEach((x, idx) => {
+    currentlyFetchedL0Tags.forEach((y, yidx) => {
+      x[1].trim().toLowerCase() === y.name.trim().toLowerCase() &&
+        l1TagsWithParentId.push({ type: x[0], name: x[2], parentId: y.id });
     });
+  });
   console.table(l1TagsWithParentId);
   console.log(
     'Total of ',
@@ -64,7 +85,11 @@ const work = async () => {
   try {
     const { code, status, error, data } = await getL0Tags(L0TagsUrl);
     if (!error) {
-      const l1TagsReadyToSend = await attachNewL1TagsWithL0Tags(data.LEVEL0);
+      const existingL1Tags = await getL1Tags(L1TagsUrl);
+      const verifiedNewL1tags = await verifyL1Tags(existingL1Tags.data.LEVEL1);
+      console.log('these are the verifiedNewL1tags to post');
+      console.table(verifiedNewL1tags);
+      const l1TagsReadyToSend = await attachNewL1TagsWithL0Tags(verifiedNewL1tags, data.LEVEL0);
       const { totalL1Tags, success, errorMessage } = await postL1Tags(
         postL1TagsUrl,
         l1TagsReadyToSend
